c83304350876c42fd846942aa7fab0ca3a1a45b8
George  Pianka <gpianka1@gmail.com>
2018-04-27 09:39:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,16 @@
 
 
 def key_for_min_value(name_hash)
-  current_name = name.hash.first
+  {current_name: current_value} = name_hash.first
+  name_hash.each do |key, value|
+    if value < current_value
+      current_value = value
+      :current_name = key
+    end
+  end
+  :current_name
+    
+  
     
 
 end
